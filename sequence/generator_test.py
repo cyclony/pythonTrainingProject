fd9a30b861d281226dceef88c3531eff5b4f9a98
Yicong <yicong.chen@qq.com>
2018-09-30 20:13:56 +0800

    update file

--- sequence/generator_test.py
+++ sequence/generator_test.py
@@ -2,24 +2,14 @@
 import collections
 def generator_test(lists):
     for x in lists:
-        if isinstance(x, collections.Iterable): yield from generator_test(x)
+        if isinstance(x, collections.Iterable): #if x is iterable the take recursive call
+            yield from generator_test(x)
         else:yield x
 
 aa = [1,2,[3,4,[3,4]]]
 
 
-a=[1,0,3,4,0,5]
-def fun():
-    yield 1
-    return
-    yield 2
-    yield 3
-
-
-
-for i,  in enumerate(aa):
-    print(i)
-
-for x in fun():
+for x in generator_test(aa):
     print(x)
+
 
