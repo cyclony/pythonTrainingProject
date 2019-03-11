4c12d40989eca2eb1901ee273ea62607d4a71e5c
Yicong <yicong.chen@qq.com>
2019-03-11 22:17:38 +0800

    update file

--- leetcode/删除链表倒数节点.py
+++ leetcode/删除链表倒数节点.py
@@ -23,14 +23,15 @@ class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         l = []
         node = head
+        if node.next == None: return None #只有一个元素的情况
         while node != None:
-            l.insert(0, node)
+            l.insert(0, node)#将链表倒序放入list中，因为只保存引用，所以空间使用效率高，只遍历一边就可以按照list[n]方式访问任意元素
             node = node.next
-        if n == len(l):#删除最后一个节点
-            l[1].next = None
-        elif n == 1: #删除第一个节点
+        if n == len(l):#删除第一个节点
             head = head.next
-        else:#删除的节点在中间的
+        elif n == 1: #删除最后一个节点
+            l[1].next = None
+        else:#删除的节点在中间的,将两边节点对接起来即可
             prev_node = l[n]
             next_node = l[n-2]
             prev_node.next = next_node
@@ -38,7 +39,7 @@ class Solution:
 
 
 head = ListNode(0)
-head.initial([1,2,3,4,5])
+head.initial([1,2])
 for i in head:
     print(i)
 print(head)
